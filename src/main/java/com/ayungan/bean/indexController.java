403badd06717d83f29e634ba265e09e0c4fa1102
alex yungan <yungan@hotmail.es>
2018-03-11 16:30:10 -0500

    init app

--- src/main/java/com/ayungan/bean/indexController.java
+++ src/main/java/com/ayungan/bean/indexController.java
@@ -5,11 +5,15 @@
  */
 package com.ayungan.bean;
 
+import static com.sun.corba.se.spi.presentation.rmi.StubAdapter.request;
+import java.io.IOException;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import org.jasig.cas.client.authentication.AttributePrincipal;
 
 /**
@@ -37,4 +41,12 @@ public class indexController {
         this.nameUser = nameUser;
     }
 
+    public void logOut() throws IOException {
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        HttpSession session = request.getSession();
+        session.invalidate();
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        //close session en cas server
+        externalContext.redirect("https://ayungan.me:8443/cas/logout");
+    }
 }
